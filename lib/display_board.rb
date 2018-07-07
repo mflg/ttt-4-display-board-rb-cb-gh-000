1de4de17886d74344623392e8a0606b4f76a3814
Marcela Fontes Lima Guerra <marcela.guerra@gmail.com>
2018-07-07 20:48:59 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,9 +4,9 @@
 # board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
 
 def display_board (board)
-  puts "   |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[4]} | #{board[5]} | #{board[6]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[7]} | #{board[8]} | #{board[9]} "
 end
